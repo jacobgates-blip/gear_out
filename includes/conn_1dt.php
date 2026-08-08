f18055ae8387f9ee396f1fabd0aa4abe5858eca9
jacobgates-blip <jacob.gates@rongotai.school.nz>
2026-08-09 10:22:34 +1200

    Use environment variable for database password
    Replaced hardcoded database password with environment variable.

--- includes/conn_1dt.php
+++ includes/conn_1dt.php
@@ -5,7 +5,7 @@
 $host = 'db';
 $dbname = 'gearout';
 $user = 'root';
-$pass = 'example';
+$pass = getenv(DB_ROOT_PASSWORD);
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
